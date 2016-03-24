7759ff291ba8afff7ab8f010395dd7441a7cfb3d
Hisham <hisham@gobolinux.org>
2016-03-24 17:22:36 -0300

    Simplify types, make differences between + and SUM more evident.

--- excel2.hs
+++ excel2.hs
@@ -12,6 +12,7 @@ data XlValue = XlNumber Double
              | XlString String
              | XlBoolean Bool
              | XlError String
+             | XlMatrix [[XlValue]]
    deriving Show
 
 data XlAddr = XlAbs Int
@@ -29,17 +30,10 @@ instance Show XlRC where
    show (XlRC r@(XlAbs rn) c@(XlAbs cn)) = "<" ++ [chr (rn + 65)] ++ show (cn + 1) ++ ">"
    show (XlRC r c) = "R" ++ show r ++ "C" ++ show c
 
-data XlArgValue = XlVal XlValue
-                | XlMtx [[XlValue]]
-   deriving Show
-
-data XlArg = XlFml XlFormula
-           | XlRng XlRC XlRC
-   deriving Show
-
 data XlFormula = XlLit XlValue
-               | XlFun String [XlArg]
+               | XlFun String [XlFormula]
                | XlRef XlRC
+               | XlRng XlRC XlRC
    deriving Show
 
 data XlCell = XlCell XlFormula
@@ -74,9 +68,31 @@ toAbs base@(XlRC br bc) cell@(XlRC cr cc) = XlRC (toAbsAddr br cr) (toAbsAddr bc
 toRC :: String -> XlRC
 toRC (l:num) = XlRC (XlAbs ((ord l) - 65)) (XlAbs ((read num) - 1))
 
-evalArg :: Set XlRC -> XlCells -> XlValues -> XlRC -> XlArg -> (XlArgValue, XlValues)
-evalArg visiting cells values rc (XlFml formula) = (XlVal newValue, newValues) where (newValue, newValues) = evalFormula (Set.insert rc visiting) cells values rc formula
-evalArg visiting cells values rc (XlRng from to) = 
+toString :: Double -> String
+toString n =
+      if fromIntegral (floor n) /= n
+      then show n
+      else show (floor n)
+
+evalFormula :: Set XlRC -> XlCells -> XlValues -> XlRC -> XlFormula -> (XlValue, XlValues)
+evalFormula _        _     values rc (XlLit value) = (value, Map.insert rc value values)
+
+evalFormula visiting cells values rc (XlRef ref')  = 
+   let
+      ref = toAbs rc ref'
+      give val = (val, Map.insert rc val values)
+   in
+      if Set.member ref visiting
+      then give (XlError "#LOOP!")
+      else 
+         case Map.lookup ref values of
+            Just v  -> give v
+            Nothing ->
+               case Map.lookup ref cells of
+                  Just (XlCell formula) -> evalFormula (Set.insert rc visiting) cells values ref formula
+                  Nothing               -> give (XlNumber 0)
+
+evalFormula visiting cells values rc (XlRng from to) = 
    let
       (XlRC (XlAbs fromR) (XlAbs fromC)) = toAbs rc from
       (XlRC (XlAbs toR)   (XlAbs toC))   = toAbs rc to
@@ -102,92 +118,72 @@ evalArg visiting cells values rc (XlRng from to) =
                   in
                      (curMtx ++ [newRow], newValues)
    in
-      (XlMtx mtx, newValues)
-
-toString :: Double -> String
-toString n =
-      if fromIntegral (floor n) /= n
-      then show n
-      else show (floor n)
-
-evalFormula :: Set XlRC -> XlCells -> XlValues -> XlRC -> XlFormula -> (XlValue, XlValues)
-evalFormula _        _     values rc (XlLit value) = (value, Map.insert rc value values)
-
-evalFormula visiting cells values rc (XlRef ref')  = 
-   let
-      ref = toAbs rc ref'
-      give val = (val, Map.insert rc val values)
-   in
-      if Set.member ref visiting
-      then give (XlError "#LOOP!")
-      else 
-         case Map.lookup ref values of
-            Just v  -> give v
-            Nothing ->
-               case Map.lookup ref cells of
-                  Just (XlCell formula) -> evalFormula (Set.insert rc visiting) cells values ref formula
-                  Nothing               -> give (XlNumber 0)
-
-evalFormula visiting cells values rc (XlFun "+" [a, b]) =
-   let
-      (va, values')  = evalArg visiting cells values  rc a
-      (vb, values'') = evalArg visiting cells values' rc b
-      
-      doSum (XlVal e@(XlError _)) _                     = e
-      doSum _                     (XlVal e@(XlError _)) = e
-      doSum (XlVal (XlNumber na)) (XlVal (XlNumber nb)) = XlNumber (na + nb)
-      doSum (XlVal (XlString sa)) (XlVal (XlNumber nb)) = XlString (sa ++ (toString nb))
-      doSum (XlVal (XlNumber na)) (XlVal (XlString sb)) = XlString ((toString na) ++ sb)
-      doSum (XlVal (XlString sa)) (XlVal (XlString sb)) = XlString (sa ++ sb)
-      doSum _                     _                     = XlError "#VALUE!"
-      val = doSum va vb
-   in
-      (val, Map.insert rc val values'')
+      (XlMatrix mtx, newValues)
 
 evalFormula visiting cells values rc (XlFun "IF" [i, t, e]) =
    let
-      (vi, valuesi) = evalArg visiting cells values  rc i
-      (vt, valuest) = evalArg visiting cells valuesi rc t
-      (ve, valuese) = evalArg visiting cells valuesi rc e
-      (XlVal vr, valuesr) = 
+      (vi, valuesi) = evalFormula (Set.insert rc visiting) cells values  rc i
+      (vt, valuest) = evalFormula (Set.insert rc visiting) cells valuesi rc t
+      (ve, valuese) = evalFormula (Set.insert rc visiting) cells valuesi rc e
+      (vr, valuesr) = 
          case vi of
-            XlVal err@(XlError _) -> (vi, valuesi)
-            XlVal (XlNumber 0)    -> (ve, valuese)
-            XlVal _               -> (vt, valuest)
-            _                     -> (XlVal (XlError "#VALUE!"), valuesi)
+            err@(XlError _) -> (vi, valuesi)
+            (XlNumber 0)    -> (ve, valuese)
+            (XlMatrix _)    -> ((XlError "#VALUE!"), valuesi)
+            _               -> (vt, valuest)
    in
       (vr, Map.insert rc vr valuesr)
 
 evalFormula visiting cells values rc (XlFun "INDIRECT" [addr]) =
    let
-      (va, valuesa) = evalArg visiting cells values rc addr
-      (XlVal vr, valuesr) = 
+      (va, valuesa) = evalFormula (Set.insert rc visiting) cells values rc addr
+      (vr, valuesr) = 
          case va of
-            XlVal (XlString computed) ->
-               evalArg visiting cells valuesa rc (XlFml (XlRef (toRC computed)))
+            XlString computed ->
+               evalFormula (Set.insert rc visiting) cells valuesa rc (XlRef (toRC computed))
             _ ->
-               (XlVal (XlError "#VALUE!"), valuesa)
+               ((XlError "#VALUE!"), valuesa)
    in
       (vr, Map.insert rc vr valuesr)
 
+evalFormula visiting cells values rc (XlFun "+" [a, b]) =
+   let
+      (va, values')  = evalFormula (Set.insert rc visiting) cells values  rc a
+      (vb, values'') = evalFormula (Set.insert rc visiting) cells values' rc b
+      
+      doSum e@(XlError _) _             = e
+      doSum _             e@(XlError _) = e
+      doSum (XlNumber na) (XlNumber nb) = XlNumber (na + nb)
+      doSum (XlString sa) (XlNumber nb) = XlString (sa ++ (toString nb))
+      doSum (XlNumber na) (XlString sb) = XlString ((toString na) ++ sb)
+      doSum (XlString sa) (XlString sb) = XlString (sa ++ sb)
+      doSum _             _             = XlError "#VALUE!"
+      
+      val = doSum va vb
+   in
+      (val, Map.insert rc val values'')
+
+
 evalFormula visiting cells values rc (XlFun "SUM" [rng]) =
    let
-      (vrng, valuesrng) = evalArg visiting cells values rc rng
-      sumValue e@(XlError _) _            = e
-      sumValue _ e@(XlError _)            = e
-      sumValue s@(XlString _) v           = v
-      sumValue v s@(XlString _)           = v
-      sumValue (XlBoolean b) (XlNumber n) = XlNumber ((if b == True then 1 else 0) + n)
-      sumValue (XlNumber n) (XlBoolean b) = XlNumber ((if b == True then 1 else 0) + n)
-      sumValue (XlNumber a) (XlNumber b)  = XlNumber (a + b)
-      s = 
+      (vrng, values') = evalFormula (Set.insert rc visiting) cells values rc rng
+      
+      doSum e@(XlError _) _            = e
+      doSum _ e@(XlError _)            = e
+      doSum s@(XlString _) v           = v
+      doSum v s@(XlString _)           = v
+      doSum (XlBoolean b) (XlNumber n) = XlNumber ((if b == True then 1 else 0) + n)
+      doSum (XlNumber n) (XlBoolean b) = XlNumber ((if b == True then 1 else 0) + n)
+      doSum (XlNumber a) (XlNumber b)  = XlNumber (a + b)
+      
+      val = 
          case vrng of
-            XlMtx mtx ->
-               foldl' (foldl' sumValue) (XlNumber 0) mtx
+            XlMatrix mtx ->
+               foldl' (foldl' doSum) (XlNumber 0) mtx
             _ ->
                XlError "#VALUE!"
    in
-      (s, Map.insert rc s valuesrng)
+      (val, Map.insert rc val values')
 
 calcCell :: Set XlRC -> XlCells -> XlValues -> XlRC -> XlCell -> XlValues
 calcCell visiting cells values rc (XlCell formula) = newValues where (_, newValues) = evalFormula visiting cells values rc formula
@@ -222,20 +218,20 @@ num n = XlLit (XlNumber n)
 cell :: String -> XlFormula
 cell a1 = XlRef (toRC a1)
 
-fun :: String -> [XlFormula] -> XlFormula
-fun name args = XlFun name (map XlFml args)
+range :: String -> String -> XlFormula
+range a1 b2 = XlRng (toRC a1) (toRC b2)
 
 main :: IO ()
 main = print $ run (XlWorksheet Map.empty)
                   [
                    (XlEvent (toRC "A1") (XlCell (num 15))),
                    (XlEvent (toRC "B1") (XlCell (num 0))),
-                   (XlEvent (toRC "A2") (XlCell (fun "+" [cell "A1", cell "B1"]))),
+                   (XlEvent (toRC "A2") (XlCell (XlFun "+" [cell "A1", cell "B1"]))),
                    (XlEvent (toRC "B1") (XlCell (cell "A1"))),
                    (XlEvent (toRC "C1") (XlCell (str "B"))),
                    (XlEvent (toRC "C2") (XlCell (num 1))),
-                   (XlEvent (toRC "B2") (XlCell (fun "INDIRECT" [fun "+" [cell "C1", cell "C2"]]))),
-                   (XlEvent (toRC "D1") (XlCell (XlFun "SUM" [XlRng (toRC "A1") (toRC "B2")]) )),
-                   (XlEvent (toRC "E1") (XlCell (XlFun "SUM" [XlRng (toRC "B1") (toRC "B2")]) )),
-                   (XlEvent (toRC "F1") (XlCell (XlFun "SUM" [XlRng (toRC "D1") (toRC "E1")]) ))
+                   (XlEvent (toRC "B2") (XlCell (XlFun "INDIRECT" [XlFun "+" [cell "C1", cell "C2"]]))),
+                   (XlEvent (toRC "D1") (XlCell (XlFun "SUM" [range "A1" "B2"]) )),
+                   (XlEvent (toRC "E1") (XlCell (XlFun "SUM" [range "B1" "B2"]) )),
+                   (XlEvent (toRC "F1") (XlCell (XlFun "SUM" [range "D1" "E1"]) ))
                   ]
