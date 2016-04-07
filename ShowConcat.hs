2ab6a9bbf1ce33d69f4bcc7e8b7946c3292d1c11
Hisham <hisham@gobolinux.org>
2016-04-07 14:39:32 -0300

    Add @@@ operator

--- ShowConcat.hs
+++ ShowConcat.hs
@@ -2,20 +2,24 @@
 {-#LANGUAGE FlexibleInstances #-}
 {-#LANGUAGE OverlappingInstances #-}
 
-module ShowConcat((@@)) where
+module ShowConcat((@@), (@@@)) where
 
 class ShowConcat a b where
    (@@) :: Show a => Show b => a -> b -> String
+   (@@@) :: Show a => Show b => a -> b -> String
 
 instance ShowConcat String String where
    (@@) a b = a ++ b
+   (@@@) a b = a ++ " " ++ b
 
 instance ShowConcat String a where
    (@@) a b = a ++ show b
+   (@@@) a b = a ++ " " ++ show b
 
 instance ShowConcat a String where
    (@@) a b = show a ++ b
+   (@@@) a b = show a ++ " " ++ b
 
 instance ShowConcat a b where
    (@@) a b = show a ++ show b
-
+   (@@@) a b = show a ++ " " ++ show b
